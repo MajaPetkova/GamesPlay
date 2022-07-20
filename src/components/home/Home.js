70662caf55079f70fd87e8bfed3498c596a4e38f
MajaPetkova <97319041+MajaPetkova@users.noreply.github.com>
2022-07-20 18:24:07 +0200

    Render latest game conditionally

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -23,9 +23,10 @@ const Home = () => {
       <img src="./images/four_slider_img01.png" alt="hero" />
       <div id="home-page">
         <h1>Latest Games</h1>
-       
-        {games.map(x=> <LatestGame game={x}/>)}
-        <p className="no-articles">No games yet</p>
+       {games.length>0 
+       ? games.map(x=> <LatestGame key={x._id} game={x}/>)
+       : <p className="no-articles">No games yet</p> }
+      
       </div>
     </section>)
 
