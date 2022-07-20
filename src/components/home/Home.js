6946c0a41c165299f932bc2bd5519ce5d0692ae4
MajaPetkova <97319041+MajaPetkova@users.noreply.github.com>
2022-07-20 18:07:15 +0200

    CreateComponents and Service

---
+++ src/components/home/Home.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import * as gameService from '../../services/gameSevice';
+
+
+const Home = () => {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    gameService.getAll()
+      .then(result => {
+        // console.log(result)
+        setGames(result)
+      })
+  }, [])
+
+  return (
+    <section id="welcome-world">
+      <div className="welcome-message">
+        <h2>ALL new games are</h2>
+        <h3>Only in GamesPlay</h3>
+      </div>
+      <img src="./images/four_slider_img01.png" alt="hero" />
+      <div id="home-page">
+        <h1>Latest Games</h1>
+       
+    
+        <p className="no-articles">No games yet</p>
+      </div>
+    </section>)
+
+}
+export default Home;
